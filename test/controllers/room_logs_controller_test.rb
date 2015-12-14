874e58e42e96de59c84cbc1718640ef437c8abda
matsuda <matsuda@lab.acs-jp.com>
2015-12-14 11:19:04 +0900

    vefify that room_logs_controller is ok

--- test/controllers/room_logs_controller_test.rb
+++ test/controllers/room_logs_controller_test.rb
@@ -5,6 +5,10 @@ class RoomLogsControllerTest < ActionController::TestCase
   def test_index
     get :index, :format => :json
     assert_response :success
+
+    json = JSON.parse(@response.body)
+    assert json.is_a?(Array)
+    assert_equal RoomLog.count, json.length
   end
 
 end
